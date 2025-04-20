40bda58d5e5ced674f260f9774eb68f575e91948
CY83R-3X71NC710N <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2025-04-20 17:58:17 -0500

    Update background.js

--- background.js
+++ background.js
@@ -2,15 +2,15 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     const url = new URL(details.url);
 
-    // Only act on direct subreddit pages (e.g., /r/something)
+    // Match subreddit root (e.g., /r/something)
     const subredditMatch = url.pathname.match(/^\/r\/([^\/]+)\/?$/);
     if (subredditMatch) {
       const subreddit = subredditMatch[1];
-      const newUrl = `https://www.reddit.com/r/${subreddit}/wiki/index`;
-      return { redirectUrl: newUrl };
+      const redirectUrl = `https://www.reddit.com/r/${subreddit}/wiki/index`;
+      return { redirectUrl };
     }
 
-    return {}; // do nothing for other URLs
+    return {};
   },
   { urls: ["*://www.reddit.com/r/*"], types: ["main_frame"] },
   ["blocking"]
